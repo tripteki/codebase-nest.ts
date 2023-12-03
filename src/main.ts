6b7286f07cc9c6f003bee98bcb870f38e3d04259
hsbmaulana <hsbmaulana.iso@protonmail.com>
2023-12-03 12:55:51 +0700

    Initial Codebase : Performance (Framework)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 "use strict";
 
 import { NestFactory, } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication, } from "@nestjs/platform-fastify";
 import { AppModule, } from "./app.module";
 
 (async () =>
 {
-    const initialization = await NestFactory.
-    create (AppModule);
+    const initialization = await NestFactory.create<NestFastifyApplication> (
+
+        AppModule,
+        new FastifyAdapter (),
+    );
 
     await initialization.
     listen (3000);
